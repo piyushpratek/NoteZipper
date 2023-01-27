5168924b36d8e2065f5139c1f5c143550fde6712
Piyush Prateek <piyushprateek231996@gmail.com>
2023-01-28 00:29:43 +0530

    created prettier.config.js in frontend

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,22 @@
-import express from 'express';
-import notes from './data/notes';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import userRoutes from './routes/userRoutes';
-import noteRoutes from './routes/noteRoutes';
-import { errorHandler, notFound } from './middlewares/errorMiddleware';
+import express from 'express'
+import notes from './data/notes'
+import dotenv from 'dotenv'
+import connectDB from './config/db'
+import userRoutes from './routes/userRoutes'
+import noteRoutes from './routes/noteRoutes'
+import { errorHandler, notFound } from './middlewares/errorMiddleware'
 // import exportError from './middlewares/errorMiddleware';
 // import { notFound } from './middlewares/errorMiddleware';
 // import errorHandler from './middlewares/errorMiddleware';
 
-const app = express();
-dotenv.config();
-connectDB();
-app.use(express.json());
+const app = express()
+dotenv.config()
+connectDB()
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('API IS RUNNING..');
-});
+  res.send('API IS RUNNING..')
+})
 
 // app.get('/api/notes', (req, res) => {
 //   res.json(notes);
@@ -27,15 +27,15 @@ app.get('/', (req, res) => {
 //   res.send(note);
 // });
 
-app.use('/api/users', userRoutes);
-app.use('/api/notes', noteRoutes);
+app.use('/api/users', userRoutes)
+app.use('/api/notes', noteRoutes)
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
 // app.use(exportError);
 // app.use(notFound);
 // app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server started on port ${PORT} `));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`server started on port ${PORT} `))
