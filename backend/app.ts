c9736421e52d92c7420a501d4bce4eb9fc9981d8
Piyush Prateek <piyushprateek231996@gmail.com>
2023-04-20 20:04:21 +0530

    added /asssets to app.ts backend

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import userRoutes from './routes/userRoutes'
 import noteRoutes from './routes/noteRoutes'
 import { errorHandler, notFound } from './middlewares/errorMiddleware'
+import path from 'path'
 
 const app = express()
 app.use(express.json())
@@ -12,6 +13,16 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes)
 app.use('/api/notes', noteRoutes)
+
+if (process.env.NODE_ENV === 'production' && process.env.VITE !== 'false') {
+  const reactBuildPath = path.join('./react-static')
+  const staticMiddleware = express.static(reactBuildPath)
+  app.use(staticMiddleware)
+  app.use('*', staticMiddleware)
+
+  const assetsPath = path.join('./react-static/assets')
+  app.use('/assets', express.static(assetsPath))
+}
 
 app.use(notFound)
 app.use(errorHandler)
