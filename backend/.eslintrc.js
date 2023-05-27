a251060be7ba0ecc6737e8c93530eb66b3923be4
Piyush Prateek <piyushprateek231996@gmail.com>
2023-05-27 18:32:01 +0530

    eslintrc file successfully executed

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,51 +1,30 @@
-// Inspired from: https://stackoverflow.com/a/64488474/10012446
+
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  env: {
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
   env: {
     browser: true,
     es2021: true,
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
-
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:jest/recommended',
-        'plugin:jest/style',
-      ],
-
-      parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
-      },
-      rules: {
-        '@typescript-eslint/no-extra-semi': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/comma-dangle': 'off',
-        '@typescript-eslint/space-before-function-paren': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/consistent-type-imports': 'off',
-        '@typescript-eslint/require-await': 'off',
-      },
-    },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  ignorePatterns: [".eslintrc.js"],
   rules: {
-    semi: 'off',
     'no-unused-vars': 'warn',
     'comma-dangle': 'off',
     'jest/expect-expect': [
@@ -53,5 +32,19 @@ module.exports = {
       { assertFunctionNames: ['expect', 'request.**.expect'] },
     ],
     'no-console': 'warn',
+    '@typescript-eslint/no-extra-semi': 'warn',
+    '@typescript-eslint/semi': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/require-await': 'off',
   },
+  overrides: [],
 }
+
